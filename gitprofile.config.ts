1fb998ffb8d42f592c5676bd7e242c11c0ea86ab
Ananya Kulkarni <142904171+AnKu-02@users.noreply.github.com>
2025-05-23 11:45:05 +0200

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -2,14 +2,14 @@
 
 const CONFIG = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (This is the only required config)
+    username: 'AnKu-02', // Your GitHub org/user name. (This is the only required config)
   },
   /**
    * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
    * If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/,
    * for example your repository is at https://github.com/arifszn/portfolio, then set base to '/portfolio/'.
    */
-  base: '/gitprofile/',
+  base: '/',
   projects: {
     github: {
       display: true, // Display GitHub projects?
@@ -17,15 +17,15 @@ const CONFIG = {
       mode: 'automatic', // Mode can be: 'automatic' or 'manual'
       automatic: {
         sortBy: 'stars', // Sort projects by 'stars' or 'updated'
-        limit: 8, // How many projects to display.
+        limit: 10, // How many projects to display.
         exclude: {
-          forks: false, // Forked projects will not be displayed if set to true.
+          forks: true, // Forked projects will not be displayed if set to true.
           projects: [], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
         },
       },
       manual: {
         // Properties for manually specifying projects
-        projects: ['arifszn/gitprofile', 'arifszn/pandora'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        projects: [''], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
     external: {
@@ -33,33 +33,33 @@ const CONFIG = {
       // To hide the `External Projects` section, keep it empty.
       projects: [
         {
-          title: 'Project Name',
+          title: '',
           description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+            '',
           imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+            '',
+          link: '',
         },
         {
-          title: 'Project Name',
+          title: '',
           description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+            '',
           imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+            '',
+          link: '',
         },
       ],
     },
   },
   seo: {
-    title: 'Portfolio of Ariful Alam',
+    title: 'Portfolio of Ananya Kulkarni',
     description: '',
     imageURL: '',
   },
   social: {
-    linkedin: 'ariful-alam',
-    x: 'arif_szn',
-    mastodon: 'arifszn@mastodon.social',
+    linkedin: '',
+    x: '',
+    mastodon: '',
     researchGate: '',
     facebook: '',
     instagram: '',
@@ -69,97 +69,144 @@ const CONFIG = {
     udemy: '',
     dribbble: '',
     behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
+    medium: '',
+    dev: '',
     stackoverflow: '', // example: '1/jeff-atwood'
     skype: '',
     telegram: '',
     website: 'https://www.arifszn.com',
-    phone: '',
-    email: 'arifulalamszn@gmail.com',
+    phone: '+49 17685982399',
+    email: 'ananyakulkarni55@gmail.com',
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'https://drive.google.com/uc?export=download&id=1xPxRXkUabmT4Rg4khBZH0ckh_Eo_b19Z', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
-    'PHP',
-    'Laravel',
-    'JavaScript',
-    'React.js',
-    'Node.js',
-    'Nest.js',
-    'MySQL',
-    'PostgreSQL',
+    // Programming Languages
+    'Python',
+    'C',
+    'C++',
+    'Go',
+    'Dart',
+
+    // Libraries & Frameworks
+    'NumPy',
+    'Pandas',
+    'PyTorch',
+    'TensorFlow',
+    'OpenCV',
+    'Scikit-learn',
+    'Matplotlib',
+    'Seaborn',
+    'MNE',
+    'YASA',
+    'FastICA',
+
+    // Tools & Technologies
     'Git',
     'Docker',
-    'PHPUnit',
-    'CSS',
-    'Antd',
-    'Tailwind',
+    'Linux',
+    'ROS 2',
+    'MATLAB',
+    'VS Code',
+    'Jupyter Notebook',
+
+    // Other Skills
+    'EEG Signal Processing',
+    'BIDS Standard',
+    'Microsoft Office',
   ],
   experiences: [
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'September 2021',
+      company: 'University of Stuttgart',
+      companyLink: 'https://www.uni-stuttgart.de',
+      position: 'Student Assistant, Infotech Buddy Program',
+      from: 'September 2023',
       to: 'Present',
-      companyLink: 'https://example.com',
+      details: [
+        'Assisted 25+ new students with administrative formalities',
+        'Organized welcome events and study groups for social engagement',
+        'Guided students on accommodation options and housing resources',
+      ],
     },
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'July 2019',
-      to: 'August 2021',
-      companyLink: 'https://example.com',
+      company: 'Erasmus Student Network',
+      companyLink: 'https://esn.de',
+      position: 'Volunteer (Events & Culture Committee)',
+      from: 'March 2024',
+      to: 'September 2024',
+      details: [
+        'Organized 10+ events including city excursions and cultural meetups',
+        'Coordinated bi-weekly planning meetings for smooth semester activities',
+        'Developed leadership and teamwork skills through event-management sessions',
+      ],
+    },
+    {
+      company: 'Rexroth Bosch',
+      companyLink: 'https://www.boschrexroth.com',
+      position: 'Werkstudent',
+      from: 'May 2023',
+      to: 'October 2023',
+      details: [
+        'Collaborated on automation projects using C++ and LabVIEW',
+        'Assisted senior engineers with data collection and analysis',
+      ],
+    },
+    {
+      company: 'GreenTeam Uni Stuttgart e.V.',
+      companyLink: 'https://greenteam-stuttgart.de',
+      position: 'Formula Student Engineer, Driverless Department (SLAM)',
+      from: 'November 2022',
+      to: 'March 2023',
+      details: [
+        'Researched and optimized SLAM algorithms (Kalman Filter, EKF)',
+        'Implemented C++ modules for mapping and localization improvements',
+        'Validated algorithm performance through real-world simulations',
+      ],
     },
   ],
   certifications: [
     {
-      name: 'Lorem ipsum',
-      body: 'Lorem ipsum dolor sit amet',
-      year: 'March 2022',
-      link: 'https://example.com',
+      name: '',
+      body: '',
+      year: '',
+      link: '',
     },
   ],
   educations: [
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2015',
-      to: '2019',
+      institution: 'University of Stuttgart',
+      degree: 'M.Sc. Infotech',
+      from: '2022',
+      to: 'Present',
     },
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2012',
-      to: '2014',
+      institution: 'Dayananda Sagar College of Engineering',
+      degree: 'B.E. Electronics and Instrumentation',
+      from: '2018',
+      to: '2022',
     },
   ],
   publications: [
     {
-      title: 'Publication Title',
+      title: 'A Comparative Study of SARS COVID-19 Detection Using X-ray and CT Scan Images Using Deep Learning Techniques',
       conferenceName: '',
-      journalName: 'Journal Name',
-      authors: 'John Doe, Jane Smith',
-      link: 'https://example.com',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    },
-    {
-      title: 'Publication Title',
-      conferenceName: 'Conference Name',
-      journalName: '',
-      authors: 'John Doe, Jane Smith',
-      link: 'https://example.com',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      journalName: 'International Journal of Health Sciences',
+      authors: 'Ananya Kulkarni',
+      link: 'https://doi.org/10.53730/ijhs.v6nS2.8170',
+      description: `
+• Utilized advanced deep learning architectures, including VGG-19, Inception V3, and ResNet, to analyze and process medical images (X-ray and CT scans).
+• Conducted analysis using metrics such as accuracy, precision, recall, and F1-score, achieving a peak accuracy of 97% for X-ray image analysis.
+• Enhanced data processing efficiency through innovative approaches in data normalization and feature extraction.
+• Published findings in the International Journal of Health Sciences (DOI: 10.53730/ijhs.v6nS2.8170).
+      `.trim(),
     },
   ],
   // Display articles from your medium or dev account. (Optional)
   blog: {
-    source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
+    source: '', // medium | dev
+    username: '', // to hide blog section, keep it empty
     limit: 2, // How many articles to display. Max is 10.
   },
   googleAnalytics: {
@@ -235,12 +282,12 @@ const CONFIG = {
 
   // Optional Footer. Supports plain text or HTML.
   footer: `Made with <a 
-      class="text-primary" href="https://github.com/arifszn/gitprofile"
+      class="text-primary" href="https://github.com/AnKu-02/gitprofile"
       target="_blank"
       rel="noreferrer"
     >GitProfile</a> and ❤️`,
 
-  enablePWA: true,
+  enablePWA: false,
 };
 
 export default CONFIG;
